b99ae519744427df015f2d0490e1cdcea32785bc
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-03-29 16:10:35 -0600

    Fix import error

--- Screens/StreamingPage.js
+++ Screens/StreamingPage.js
@@ -1,23 +1,21 @@
-import {View, Text, StyleSheet} from 'react-native'
-import LiveMap from '../Components/LiveMap';
-import LiveStream from '../Components/LiveStream';
+import {View, Text, StyleSheet} from 'react-native';
+import {LiveStream} from '../Components/LiveStream';
 
 const StreamingPage = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Streaming Page</Text>
-            <LiveMap />
-            <LiveStream />
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text>Streaming Page</Text>
+      <LiveStream />
+    </View>
+  );
+};
 
 export default StreamingPage;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
