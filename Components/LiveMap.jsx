a6e5a7d31b1a8c6bb8e16dc535478f40f6d06081
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-12 15:22:08 -0600

    Add functionality for sending data to backend

--- Components/LiveMap.jsx
+++ Components/LiveMap.jsx
@@ -4,36 +4,36 @@ import MapView, {Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import firebase from '@react-native-firebase/app';
 import database from '@react-native-firebase/database';
+import {get} from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
-export default function LiveMap() {
-  
+export default function LiveMap(tripCoordinates, setTimestamps) {
   if (!firebase.apps.length) {
     firebase.initializeApp();
   }
-  
+
   const [region, setRegion] = useState(null);
   const [locations, setLocations] = useState([]);
-  
+
   useEffect(() => {
     // Request location permission
     Geolocation.requestAuthorization();
-    
+
     // Set up a watcher to capture the longitude and latitude every 10 seconds
     const watchId = Geolocation.watchPosition(
       position => {
-        const {latitude, longitude, speed, heading, timestamp, altitude} =
-        position.coords;
-        
+        const {latitude, longitude, heading} = position.coords;
+        const timestamp = new Date(position.timestamp).toISOString();
+
         // Update the locations array with the new location
         setLocations(prevLocations => [
           ...prevLocations,
-          {latitude, longitude, speed, heading},
+          {latitude, longitude, heading},
         ]);
         // Update the map region to the current location
         // setRegion({
         //   latitude,
         //   longitude,
-          
+
         //   latitudeDelta: 0.0922,
         //   longitudeDelta: 0.0421,
         // });
@@ -42,32 +42,42 @@ export default function LiveMap() {
         locationRef.push({
           latitude,
           longitude,
+          timestamp,
         });
       },
       error => console.log(error.message),
       {
         enableHighAccuracy: true,
         distanceFilter: 10,
-        interval: 10000,
-        fastestInterval: 5000,
+        interval: 1000,
+        fastestInterval: 1000,
       },
-      );
-      
-       // Set up a listener for real-time changes to the 'locations' node
-       const onValueChange = database()
-       .ref('locations')
-       .on('value', snapshot => {
-          const newLocations = snapshot.val();
-          // console.log(newLocations);
+    );
 
-          if (newLocations) {
-      
+    // Set up a listener for real-time changes to the 'locations' node
+    const onValueChange = database()
+      .ref('locations')
+      .on('value', snapshot => {
+        const newLocations = snapshot.val();
+
+        if (newLocations) {
+          const locationValues = Object.values(newLocations);
+          const locationsArray = locationValues.map(location => [
+            location.latitude,
+            location.longitude,
+          ]);
+          const timestamps = locationValues.map(location => location.timestamp);
+          setTripCoordinates(locationsArray);
+          setTimestamps(timestamps);
+        }
+
+        if (newLocations) {
           // Convert the object to an array of its values
           const locationsArray = Object.values(newLocations);
-      
+
           // Get the last location object from the array
           const lastLocation = locationsArray[locationsArray.length - 1];
-      
+
           // Update the region state with the latitude and longitude of the last location
           setRegion({
             latitude: lastLocation.latitude,
@@ -75,7 +85,7 @@ export default function LiveMap() {
             latitudeDelta: 0.0015,
             longitudeDelta: 0.0015,
           });
-          }
+        }
       });
 
     // Clean up the watcher and listener when the component unmounts
@@ -83,16 +93,7 @@ export default function LiveMap() {
       Geolocation.clearWatch(watchId);
       database().ref('locations').off('value', onValueChange);
     };
- }, []);
-    
-    const logTrip = () => {
-      console.log('Trip Locations:', locations);
-    };
-
-  // Log the trip every time the locations state changes
-  // useEffect(() => {
-  //   logTrip();
-  // }, [locations]);
+  }, []);
 
   return (
     <View>
