09f5b7f3967ac964791d091172cffe4575cfccaf
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-16 14:35:34 -0600

    Refactor function for getting recent location

--- Screens/ViewerPage.js
+++ Screens/ViewerPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import firebase from '@react-native-firebase/app';
 import database from '@react-native-firebase/database';
@@ -12,28 +12,54 @@ function ViewerPage({tripCoordinates}) {
   }
 
   //logs the streamer's location
-  useEffect(() => {
-    streamerLocation && console.log(streamerLocation);
-  }, [streamerLocation]);
+  // useEffect(() => {
+  //   streamerLocation && console.log(streamerLocation);
+  // }, [streamerLocation]);
 
   useEffect(() => {
-    const onValueChange = database()
-      .ref('locations')
-      .on('value', snapshot => {
-        const newLocations = snapshot.val();
+    // const onValueChange = database()
+    //   .ref('locations')
+    //   .on('value', snapshot => {
+    //     const newLocations = snapshot.val();
 
-        if (newLocations) {
-          const locationsArray = Object.values(newLocations);
-          const lastLocation = locationsArray[locationsArray.length - 1];
-          setStreamerLocation(lastLocation);
-        }
-      });
+    //     if (newLocations) {
+    //       const locationsArray = Object.values(newLocations);
+    //       const lastLocation = locationsArray[locationsArray.length - 1];
+    //       setStreamerLocation(lastLocation);
+    //     }
+    //   });
 
-    // Cleanup function
-    return () => {
-      database().ref('locations').off('value', onValueChange);
-    };
-  }, [streamerLocation]);
+    // // Cleanup function
+    // return () => {
+    //   database().ref('locations').off('value', onValueChange);
+    // };
+
+    const fetchLastLocation = () => {
+      database()
+         .ref('locations')
+         .orderByChild('timestamp')
+         .limitToLast(1)
+         .once('value')
+         .then(snapshot => {
+           const newLocations = snapshot.val();
+           if (newLocations) {
+             // Since limitToLast(1) returns an object, we need to extract the value
+             const lastLocation = Object.values(newLocations)[0];
+             setStreamerLocation(lastLocation);
+           }
+         });
+     };
+     
+
+    // Fetch the last location immediately
+    fetchLastLocation();
+
+    // Set an interval to fetch the last location every 5 seconds (or any desired interval)
+    const intervalId = setInterval(fetchLastLocation, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -53,7 +79,12 @@ function ViewerPage({tripCoordinates}) {
             }}
             title={"Streamer's Location"}
             description={'This is the location of the streamer'}
-          />
+          >
+            <Image
+              source={require('../assets/marker.png')}
+              style={styles.markerImage}
+            />
+          </Marker>
         </MapView>
       )}
       <View>
@@ -75,4 +106,8 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '50%',
   },
+  markerImage: {
+    height: 100,
+    width: 100,
+  }
 });
